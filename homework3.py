85b2b28602211c61ad0549765a8d74c50cbbc6a2
Nicu <nrigheriu@gmail.com>
2025-06-05 00:08:11 +0200

    Add files via upload
    run with e.g. homework3.py --year 2023 --month 3

---
+++ homework3.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+from pathlib import Path
+
+import pandas as pd
+import xgboost as xgb
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import root_mean_squared_error
+from prefect import task, flow
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import numpy as np
+
+import mlflow
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("nyc-taxi-experiment")
+
+models_folder = Path('models')
+models_folder.mkdir(exist_ok=True)
+
+
+@task(name="read_dataframe")
+def read_dataframe(year:int, month:int) -> pd.DataFrame:
+    url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet'
+    df = pd.read_parquet(url)
+    print(f"Read {len(df)} rows from {url}")
+    print(f"DataFrame size: {df.size}")
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
+    print(f"Rows remaining after filtering: {len(df)}.")
+    return df
+
+@task(name="create_X")
+def create_X(df, dv=None):
+    categorical = ['PU_DO']
+    numerical = ['trip_distance']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+
+    if dv is None:
+        dv = DictVectorizer(sparse=True)
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+
+    return X, dv
+
+@task(name="create_X_linear_regression")
+def create_X_linear_regression(df, dv=None):
+    categorical = ['PULocationID', 'DOLocationID']
+    numerical = ['trip_distance']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+
+    if dv is None:
+        dv = DictVectorizer(sparse=True)
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+
+    return X, dv
+
+@task(name="train_model")
+def train_model(X_train, y_train, X_val, y_val, dv):
+    with mlflow.start_run() as run:
+        train = xgb.DMatrix(X_train, label=y_train)
+        valid = xgb.DMatrix(X_val, label=y_val)
+
+        best_params = {
+            'learning_rate': 0.09585355369315604,
+            'max_depth': 30,
+            'min_child_weight': 1.060597050922164,
+            'objective': 'reg:linear',
+            'reg_alpha': 0.018060244040060163,
+            'reg_lambda': 0.011658731377413597,
+            'seed': 42
+        }
+
+        mlflow.log_params(best_params)
+
+        booster = xgb.train(
+            params=best_params,
+            dtrain=train,
+            num_boost_round=30,
+            evals=[(valid, 'validation')],
+            early_stopping_rounds=50
+        )
+
+        y_pred = booster.predict(valid)
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+
+        with open("models/preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+
+        return run.info.run_id
+
+@task(name="train_linear_regression")
+def train_linear_regression(X_train, y_train, X_val, y_val, dv):    
+    with mlflow.start_run() as run:
+        mlflow.set_tag("model_type", "linear_regression")
+        mlflow.log_params({"model": "LinearRegression"})
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_val)
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred))
+        mlflow.log_metric("rmse", rmse)
+        mlflow.sklearn.log_model(model, artifact_path="linear_regression_model")
+        
+        print(f"RMSE: {rmse}")
+        print(f"Model intercept: {model.intercept_}")
+
+        return model, dv, run.info.run_id
+
+@flow(name="run_duration_prediction")
+def run(year, month):
+    df_train = read_dataframe(year=year, month=month)
+
+    next_year = year if month < 12 else year + 1
+    next_month = month + 1 if month < 12 else 1
+    df_val = read_dataframe(year=next_year, month=next_month)
+
+    X_train, dv = create_X_linear_regression(df_train)
+    X_val, _ = create_X_linear_regression(df_val, dv)
+
+    target = 'duration'
+    y_train = df_train[target].values
+    y_val = df_val[target].values
+
+    #run_id = train_model(X_train, y_train, X_val, y_val, dv)
+    model, dv, run_id = train_linear_regression(X_train, y_train, X_val, y_val, dv)
+    print(f"MLflow run_id: {run_id}")
+    return run_id
+
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Train a model to predict taxi trip duration.')
+    parser.add_argument('--year', type=int, required=True, help='Year of the data to train on')
+    parser.add_argument('--month', type=int, required=True, help='Month of the data to train on')
+    args = parser.parse_args()
+
+    run_id = run(year=args.year, month=args.month)
+
+    with open("run_id.txt", "w") as f:
+        f.write(run_id)
